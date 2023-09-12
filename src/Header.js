47d64db3a762207c547ec02cd2d3b20c0b6415e5
Stefano Santo <s.santo2004@gmail.com>
2023-09-12 17:10:47 -0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+    return(
+        <div className='holder'>
+        <img src="" alt="Transportes X"  width="100"/> 
+        <h1>Transportes X</h1>
+         </div>
+    )    
+}
+
+export default Header
